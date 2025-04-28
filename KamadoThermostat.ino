aa2a8ff6a92317ae07bc9b76953b81585edece43
gitramalama <chris.ramsey.ramalama@gmail.com>
2025-04-28 15:12:43 -0600

    Added pushbutton

--- KamadoThermostat.ino
+++ KamadoThermostat.ino
@@ -16,6 +16,9 @@
 #define PULSE_INTERVAL 400  // ms
 #define PULSES_PER_CYCLE 15
 
+// pushbutton
+#define DEBOUNCE_INTERVAL 20  // ms
+
 // instantiate matrix object
 ArduinoLEDMatrix matrix;
 
@@ -23,18 +26,44 @@ ArduinoLEDMatrix matrix;
 bool stateLEDBUILTIN;
 bool stateLEDEXTERNAL;
 unsigned long timeLastBlink = 0;
+bool statePBwas;
+unsigned long timeLastDebounce = 0;
 
 void setup() {
   Serial.begin(9600);
 
-  pinMode(LED_BUILTIN, OUTPUT);  // setup of builtin LED
-  pinMode(LEDEXTERNAL_PIN, OUTPUT); // setup of external LED
+  pinMode(LED_BUILTIN, OUTPUT);      // setup of builtin LED
+  pinMode(LEDEXTERNAL_PIN, OUTPUT);  // setup of external LED
+  pinMode(PB_PIN, INPUT_PULLUP);     // setup of pushbutton
 }
 
 void loop() {
   // loop variables
   bool isTimeToBlink = false;                        // flag for blinking/updating lights
   unsigned int lightsInterval = PULSE_INTERVAL * 2;  // LED blink/update rate
+  bool statePB;
+  bool statePBis = digitalRead(PB_PIN);
+  bool isPBpressed;
+
+  // check... did the pushbutton remain unchanged?
+  if (statePBis == statePBwas) {
+    // pushbutton remained unchanged; do nothing
+  } else {
+    // pushbutton changed; reset debounce timer
+    timeLastDebounce = millis();
+  }
+  // check... has pushbutton state been the same for awhile?
+  if (millis() - timeLastDebounce > DEBOUNCE_INTERVAL) {
+    // pushbutton state has been the same for awhile; set pushbutton state
+    statePB = statePBis;
+    // check pushbutton state... is it low or high?
+    if (statePB == LOW || statePB == HIGH) {
+      // pushbutton state is low or high; set variable for actual pushbutton state
+      stateLEDEXTERNAL = !statePB;  // opposite due to invoking input pullup resistor
+      isPBpressed = !statePB;       // opposite due to invoking input pullup resistor
+    }
+    digitalWrite(LEDEXTERNAL_PIN, stateLEDEXTERNAL);
+  }
 
   // check... time to update to lights?
   if (millis() - timeLastBlink > lightsInterval) {
@@ -47,10 +76,6 @@ void loop() {
     // blink LED builtin
     stateLEDBUILTIN ^= 1;
     digitalWrite(LED_BUILTIN, stateLEDBUILTIN);
-
-    // blink external LED
-    stateLEDEXTERNAL ^= 1;
-    digitalWrite(LEDEXTERNAL_PIN, stateLEDEXTERNAL);
   }
 
   // check... lights flag set?
@@ -58,4 +83,7 @@ void loop() {
     // lights blink/update flag is set; reset timer
     timeLastBlink = millis();
   }
+
+  // reset pushbutton debouncer
+  statePBwas = statePBis;
 }
